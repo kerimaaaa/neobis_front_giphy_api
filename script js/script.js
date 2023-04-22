d535453e20f1f6e2e0d6f2f3c8d666ede16f0f69
kerimaaaa <119394857+kerimaaaa@users.noreply.github.com>
2023-04-22 17:27:40 +0300

    giphy

---
+++ script js/script.js
@@ -0,0 +1,39 @@
+//api.giphy.com/v1/gifs/search?q=&api_key=JSewDw2NG8qKekya6PqHYlJYQTzPthUw&limit=10
+const searchBtn = document.querySelector('#search-btn');
+const url = `https://api.giphy.com/v1/gifs/search`;
+let search = `?q=`;
+const key = "&api-key=JSewDw2NG8qKekya6PqHYlJYQTzPthUw";
+const limitation = `&limit=40`;
+
+let q = "";
+
+let urlComplete = "";
+
+
+
+
+searchBtn.onclick = () => {
+    q = document.getElementById('search').value;
+    urlComplete = url + search + q + key + limitation;
+    getData();
+}
+
+const getData = async () => {
+
+    await fetch(urlComplete).then((response) => {
+        return response.json();
+    }).then((giphy) => {
+
+
+        for (let i = 0; i < giphy.data.length; i++) {
+            const gif = document.createElement('img');
+
+            gif.src = giphy.data[i].images["original"].url;
+            gif.className = "gif-img";
+            const portfolio = document.getElementById("portfolio");
+            portfolio.appendChild(gif);
+        }
+    })
+}
+
+getData();
